67f992d0993eb327793035ef4b6306cf484883c6
diegoVillaDur <164115110+diegoVillaDur@users.noreply.github.com>
2026-03-03 19:52:16 -0600

    changed time stuff

--- mainnpc.py
+++ mainnpc.py
@@ -8,16 +8,16 @@ print("Meta", encontrar_posicion(2), "\n")
 
 
 # --------------Buscar por BFS
+t_inicio = time.perf_counter()
 if bfs():
     print("Camino encontrado por BFS:")
     for paso in bfs():
         print(paso)
     print(f"Meta encontrada\nLongitud del camino: {len(bfs()) - 1} pasos\n")
-
-
 else:
     print("No se encontró un camino.")
-
+t_fin = time.perf_counter()
+print("Tiempo:", t_fin - t_inicio)
 
 # --------------Buscar por DFS
 print("\nCamino encontrado por DFS:")
@@ -27,4 +27,4 @@ camino_encontrado = dfs(laberinto, encontrar_posicion("s"), encontrar_posicion(2
 t_fin = time.perf_counter()
 
 print(f"Longitud del camino: {len(camino_encontrado) - 1} pasos")
-print("\nTiempo:", t_fin - t_inicio)
+print("Tiempo:", t_fin - t_inicio)
